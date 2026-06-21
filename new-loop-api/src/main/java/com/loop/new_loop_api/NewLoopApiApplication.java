3e74c55bc73a471d1ce0e494f1b79224ef70e5fb
Gonzalo Winazki <gwinazki@eljumillano.com.ar>
2026-06-21 00:12:08 -0300

    chore: initial project setup

---
+++ new-loop-api/src/main/java/com/loop/new_loop_api/NewLoopApiApplication.java
@@ -0,0 +1,13 @@
+package com.loop.new_loop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewLoopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewLoopApiApplication.class, args);
+	}
+
+}
---
+++ new-loop-api/src/test/java/com/loop/new_loop_api/NewLoopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loop.new_loop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewLoopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
